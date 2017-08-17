65a1e9eed1fa20641dfa49c15815e81e6497d05b
lizhiyuan <148334733@qq.com>
2017-08-17 10:27:11 +0800

    数据库配置成功

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
+//引入数据库配置文件
+var setting = require('./setting');
+//临时存放一些数据
+var flash = require('connect-flash');
+//支持会话
+var session = require('express-session');
+//把会话保存在mongodb中去
+var Mongostore = require('connect-mongo')(session);
 
 var app = express();
 
@@ -20,7 +27,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+//使用flash
+app.use(flash());
+//将session保存在MongoDB里面
+app.use(session({
+    secret:"lzyBlog",
+    key:setting.db,
+    cookie:{maxAge:30 * 24 * 60 * 60 * 1000},
+    store:new Mongostore({
+        url:'mongodb://localhost/lzyblog'
+    }),
+    resave:false,
+    saveUninitialized:true
+}))
 routes(app)
 
 // catch 404 and forward to error handler
